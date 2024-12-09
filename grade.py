eb16924a5af9370a0c68513e05c15c7808985c52
davis <chiraodavis@gmail.com>
2024-12-09 14:04:25 +0300

    first commit

---
+++ grade.py
@@ -0,0 +1,19 @@
+score = int(input("Enter Your Score to get Grade: "))
+
+if score >= 80 and score <= 100:
+    print("Grade A")
+
+elif score >= 70 and score <= 80:
+    print("Grade B")
+
+elif score >= 60 | score <= 50:
+    print("Grade C")
+
+elif score >= 50 | score <= 60:
+    print("Grade C")
+
+elif score >= 40 | score <= 50:
+    print("Grade D")
+
+else:
+    print("grade E")               
